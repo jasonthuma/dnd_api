ddc562766b46cff3b9ea417ac6333408fdef9b2c
Jason Thuma <jasonthuma@gmail.com>
2022-06-14 11:26:10 -0400

    changed db settings

--- knex.js
+++ knex.js
@@ -1,10 +1,21 @@
+// module.exports = require("knex")({
+//   client: "mysql2",
+//   connection: {
+//     host: process.env.MYSQL_HOST,
+//     user: process.env.MYSQL_USER,
+//     password: process.env.MYSQL_PASSWORD,
+//     database: process.env.MYSQL_DB_NAME,
+//     port: process.env.MYSQL_PORT || 3306,
+//   },
+// });
+
 module.exports = require("knex")({
   client: "mysql2",
   connection: {
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DB_NAME,
+    host: "us-cdbr-east-05.cleardb.net",
+    user: "b1aff09bf62ad2",
+    password: "d32eb8d7",
+    database: "heroku_f0f3fbf36e151e4",
     port: process.env.MYSQL_PORT || 3306,
   },
 });
